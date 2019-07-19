7dd0084afb56bd6da4b755d65243d7bfd9ce5960
quhongqiang <1256618908@qq.com>
2019-07-19 17:15:46 +0800

    first commit

---
+++ config-overrides.js
@@ -0,0 +1,71 @@
+
+/**
+ * [description]
+ * 使用过create-react-app（以下简称cra）的朋友都知道，这是react官方的一款脚手架工具，
+ * 使用过内部集成了使用react-app-rewired,使用此插件可以暴露出webpack
+ * 但是！react-app-rewired2.x以后，不再支持injectBabelPlugin的方式，需要安装customize-cra。
+ * npm install customize-cra --save-dev 或者  yarn add customize-cra --dev
+ * 所以新建config-overrides.js可以读取到该文件
+ */
+const { override, fixBabelImports,addPostcssPlugins } = require('customize-cra');
+
+/**
+ * [关闭打包后的sourcemap description]
+ * [打包后我们会发现静态文件夹中会有很多的css和js的map文件，关闭sourcemap]
+ */
+// process.env.GENERATE_SOURCEMAP = "false";
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd-mobile',
+    style: 'css',
+  }),
+  addPostcssPlugins([require('postcss-pxtorem')({
+                               rootValue: 16,
+                               propList: ['*']
+                               // propList: ['*', '!border*', '!font-size*', '!letter-spacing'],
+                               // propWhiteList: []
+                            }),])
+
+    /**
+     * [安装less]
+     * npm i less
+     * npm i -D less-loader
+     */
+    // addLessLoader({
+    //   javascriptEnabled: true,
+    //   modifyVars: { '@primary-color': '#1DA57A' },
+    // }),
+
+    /**
+     * [remUnit description]
+     * npm install lib-flexible --save
+     * flexible会为页面根据屏幕自动添加<meta name='viewport' >标签，
+     * 动态控制initial-scale，maximum-scale，minimum-scale等属性的值。
+     * 安装完成之后再入口index.js页面 import lib-flexible
+     * 注意事项（important）: 由于flexible会动态给页面header中添加<meta name='viewport' >标签，
+     * 所以务必请把目录 public/index.html 中的这个标签删除！！！
+     */
+
+     /**
+      * [postcss-px2rem]
+      * npm install postcss-px2rem --save
+      * postcss-px2rem会将px转换为rem，rem单位用于适配不同宽度的屏幕，
+      * 根据<html>标签的font-size值来计算出结果，1rem=html标签的font-size值。
+      */
+
+    // addPostcssPlugins([
+    //   require("postcss-px2rem")({ remUnit: 37.5 })
+    // ])
+
+    /**
+     * [description]
+     * 温馨提示： remUnit这个配置项的数值是多少呢？？？ 通常我们是根据设计图来定这个值，原因很简单，便于开发。
+     * 假如设计图给的宽度是750，我们通常就会把remUnit设置为75，这样我们写样式时，可以直接按照设计图标注的宽高来1:1还原开发。
+     * 那为什么你在这里写成了37.5呢？？？那我们后面专门来讲！
+     * 之所以设为37.5，是为了引用像mint-ui这样的第三方UI框架，因为第三方框架没有兼容px2rem ，
+     * 将remUnit的值设置为设计图宽度（这里为750px）75的一半，即可以1:1还原mint-ui的组件，否则会样式会有变化，例如按钮会变小。
+     * 既然设置成了37.5 那么我们必须在写样式时，也将值改为设计图的一半。
+     */
+ );
+
--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import './rem.js';
 
 function App() {
+  const [bg] = useState(2);
+  const [year, setYear] = useState(2019);
+  const [month, setMonth] = useState(1);
+  const [day, setDay] = useState(1);
+  const [week, setWeek] = useState(1);
+  const [hour, setHour] = useState(12);
+  const [minute, setMinute] = useState(1);
+  const [second, setSecond] = useState(1);
+
+  useEffect(() => {
+    setTimeout(()=>{
+      let time = new Date();
+      setYear(time.getFullYear());
+      setMonth(time.getMonth()+1);
+      setDay(time.getDate());
+      setWeek(time.getDay());
+      setHour(time.getHours());
+      setMinute(time.getMinutes());
+      setSecond(time.getSeconds());
+    },1000)
+    // setTimeout(() => {
+    //   bg === 10 ? setBg(1): setBg(bg+1)
+    // }, 30000)
+  })
+  var yearDay = new Date().getFullYear();
+  var monthDay = new Date().getMonth()+1;
+
+  function getDays(year, month){ //获取当月的天数
+    month = parseInt(month,10);
+    var d= new Date(year,month,0);
+    return d.getDate()
+  }
+  // 数字大小写转换
+  function numToBig(num) {
+    var zh = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十"];
+    var res = "";
+    if (num <= 10) {
+      res = zh[num];
+    } else if (num < 20) {
+      var bits = num % 10;
+      res = zh[10] + zh[bits];
+    } else if (num < 70) {
+      bits = num % 10;
+      var decade = parseInt(num / 10);
+      if (bits === 0) {
+          res = zh[decade] + zh[10];
+      } else {
+          res = zh[decade] + zh[10] + zh[bits];
+      }
+    }
+    return res;
+  }
+  function array(length) {
+    return Array.from({length}).map((v, k) => k).map( x => x + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      backgroundImage: `url(${require(`./assets/${bg}.jpg`)})`,
+      backgroundSize: 'cover',
+    }}>
+      <div className='box'>
+        <div className='year item'>
+          <span>{year}</span>
+        </div>
+        {array(12).map((item,index)=>{
+          return (
+            <div key={index} className={`month item ${index===(month-1)?"active":""}`} style={{transform: `rotate(${30*(month-1)-index*30}deg)`}}>
+              {`${numToBig(item)}月`}
+            </div>
+          )
+        })}
+
+        {array(getDays(yearDay, monthDay)).map((item,index)=>{
+          return (
+            <div key={index} className={`day item ${index===(day-1)?"active":""}`} style={{transform: `rotate(${(360/getDays(yearDay, monthDay))*(day-1)-index*(360/getDays(yearDay, monthDay))}deg)`}}>
+              {`${numToBig(item)}日`}
+            </div>
+          )
+        })}
+
+        {array(7).map((item,index)=>{
+          return (
+            <div key={index} 
+            className={`week item ${index===(week-1)?"active":""}`} 
+            style={{transform: `rotate(${(360/7)*(week-1)-index*(360/7)}deg)`}}>
+              {`星期${numToBig(item)}`}
+            </div>
+          )
+        })}
+
+        {array(24).map((item,index)=>{
+          return (
+            <div key={index} 
+            className={`hour item ${index===(hour-1)?"active":""}`} 
+            style={{transform: `rotate(${(360/24)*(hour-1)-index*(360/24)}deg)`}}>
+              {`${numToBig(item)}点`}
+            </div>
+          )
+        })}
+        {array(60).map((item,index)=>{
+          return (
+            <div key={index} 
+            className={`minute item ${index===(minute-1)?"active":""}`} 
+            style={{transform: `rotate(${(360/60)*(minute-1)-index*(360/60)}deg)`}}>
+              {`${numToBig(item)}分`}
+            </div>
+          )
+        })}
+        {array(60).map((item,index)=>{
+          return (
+            <div key={index} 
+            className={`second item ${index===(second-1)?"active":""}`} 
+            style={{transform: `rotate(${(360/60)*(second-1)-index*(360/60)}deg)`}}>
+              {`${numToBig(item)}秒`}
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
